a41cc3364de5f0c3b18fcbabbfe3264bc51d896a
Brayam Mazuera <brayanmazuera1@gmail.com>
2024-10-01 10:45:21 +0800

    include the class

--- ServerApp/ServerApp/Program.cs
+++ ServerApp/ServerApp/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.ServiceModel;
+using AstroMath;
+
+namespace ServerApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Server App Started...");
+
+			AstroMathFunctions mathFunctions = new AstroMathFunctions();
+
+			double velocity = mathFunctions.StarVelocity(500.1, 500.0);
+			Console.WriteLine($"Star Velocity: {velocity} m/s");
+
+			double distance = mathFunctions.StarDistance(0.547);
+			Console.WriteLine($"Star Distance: {distance} parsecs");
+
+			double kelvin = mathFunctions.Kelvin(27);
+			Console.WriteLine($"Temperature: {kelvin} K");
+
+			double eventHorizon = mathFunctions.EventHorizon(8.2 * Math.Pow(10, 36));
+			Console.WriteLine($"Event Horizon: {eventHorizon} meters");
+
+			Console.ReadLine();
+		}
+	}
+}
