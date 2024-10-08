e828eb5a0361cf5badc85aa2e198d9180a0150d3
Brayam Mazuera <brayanmazuera1@gmail.com>
2024-10-08 11:24:16 +0800

    updated server

--- ServerApp/ServerApp/obj/Debug/net8.0/ServerApp.AssemblyInfo.cs
+++ ServerApp/ServerApp/obj/Debug/net8.0/ServerApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a41cc3364de5f0c3b18fcbabbfe3264bc51d896a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
