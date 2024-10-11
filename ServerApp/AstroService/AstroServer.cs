08c778fe9fc110a0a7d48cbe782a6ee3d4ee95e4
Brayam Mazuera <brayanmazuera1@gmail.com>
2024-10-11 14:59:15 +0800

    update server app

--- ServerApp/AstroService/AstroServer.cs
+++ ServerApp/AstroService/AstroServer.cs
@@ -8,7 +8,6 @@ using AstroMaths;
 
 namespace AstroService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
     public class AstroServer : IAstroContract
     {
         public double CalculateStarVelocity(double observedWavelength, double restWavelength)
